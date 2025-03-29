e7934e3678b2f1946c23e627b28295ae96c261f4
Madjakul <francis.kulumba@inria.fr>
2025-03-29 21:00:30 +0100

    doc(clm_loss.py): document the class

--- deep_stylometry/modules/clm_loss.py
+++ deep_stylometry/modules/clm_loss.py
@@ -6,12 +6,24 @@ import torch.nn.functional as F
 
 
 class CLMLoss(nn.Module):
+    """Cross-entropy loss for causal language modeling."""
+
     def forward(
         self,
         logits: torch.Tensor,
         input_ids: torch.Tensor,
         attention_mask: torch.Tensor,
     ):
+        """Compute the cross-entropy loss for causal language modeling.
+
+        Args:
+            logits: (batch_size, seq_len, vocab_size)
+            input_ids: (batch_size, seq_len)
+            attention_mask: (batch_size, seq_len)
+
+        Returns:
+            torch.Tensor: The cross-entropy loss.
+        """
         shift_logits = logits[..., :-1, :].contiguous()
         shift_labels = input_ids[..., 1:].contiguous()
         shift_mask = attention_mask[..., 1:].contiguous()
