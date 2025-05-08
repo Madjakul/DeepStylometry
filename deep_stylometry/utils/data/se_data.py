d705d1e1bee89896aa62b21991c5b7ff03fa607e
Madjakul <Madjakul@users.noreply.github.com>
2025-05-08 15:21:06 +0000

    refactor: apply code formatting with Black, isort, and docformatter

--- deep_stylometry/utils/data/se_data.py
+++ deep_stylometry/utils/data/se_data.py
@@ -7,9 +7,8 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader
 from transformers import DataCollatorForLanguageModeling
 
-from deep_stylometry.utils.data.custom_data_collator import (
-    CustomDataCollatorForLanguageModeling,
-)
+from deep_stylometry.utils.data.custom_data_collator import \
+    CustomDataCollatorForLanguageModeling
 from deep_stylometry.utils.helpers import get_tokenizer
 
 
