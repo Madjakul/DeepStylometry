88522a6b9c7b3d6304193c7df7c2de3417e44129
Madjakul <Madjakul@users.noreply.github.com>
2025-05-12 08:42:19 +0000

    refactor: apply code formatting with Black, isort, and docformatter

--- deep_stylometry/utils/train_utils.py
+++ deep_stylometry/utils/train_utils.py
@@ -2,16 +2,13 @@
 
 from typing import Any, Dict, Optional
 
+import lightning as L
 import psutil
-from lightning.pytorch.callbacks import (
-    EarlyStopping,
-    LearningRateMonitor,
-    ModelCheckpoint,
-)
+from lightning.pytorch.callbacks import (EarlyStopping, LearningRateMonitor,
+                                         ModelCheckpoint)
 from lightning.pytorch.loggers import CSVLogger, WandbLogger
 from ray.tune.integration.pytorch_lightning import TuneReportCheckpointCallback
 
-import lightning as L
 from deep_stylometry.modules import DeepStylometry
 from deep_stylometry.utils.data.halvest_data import HALvestDataModule
 from deep_stylometry.utils.data.se_data import SEDataModule
