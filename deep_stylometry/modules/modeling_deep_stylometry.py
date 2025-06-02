5eb1762908610e80a189b10a1debfb47c02f6fb6
Madjakul <Madjakul@users.noreply.github.com>
2025-06-02 09:10:34 +0000

    refactor: apply code formatting with Black, isort, and docformatter

--- deep_stylometry/modules/modeling_deep_stylometry.py
+++ deep_stylometry/modules/modeling_deep_stylometry.py
@@ -7,13 +7,8 @@ import lightning as L
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchmetrics.classification import (
-    AUROC,
-    BinaryAUROC,
-    BinaryF1Score,
-    BinaryPrecision,
-    BinaryRecall,
-)
+from torchmetrics.classification import (AUROC, BinaryAUROC, BinaryF1Score,
+                                         BinaryPrecision, BinaryRecall)
 from transformers import get_cosine_schedule_with_warmup
 
 from deep_stylometry.modules.info_nce_loss import InfoNCELoss
