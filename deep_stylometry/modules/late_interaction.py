fix: increase late interaction scaling factor

deep_stylometry/modules/late_interaction.py
@@ -62,7 +62,7 @@ class LateInteraction(nn.Module):
             distance = (positions.unsqueeze(1) - positions.unsqueeze(0)).abs().float()
             self.register_buffer("distance", distance)
         self.exp_decay = exp_decay
-        self.logit_scale = nn.Parameter(torch.log(torch.tensor(1.0)).unsqueeze(0))
+        self.logit_scale = nn.Parameter(torch.log(torch.tensor(10.0)).unsqueeze(0))
 
     @property
     def alpha(self):

deep_stylometry/modules/modeling_deep_stylometry.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 from torcheval.metrics import BinaryAUROC, HitRate, ReciprocalRank
 from transformers import get_cosine_schedule_with_warmup
 
+from deep_stylometry.modules.contrastive_loss import ContrastiveLoss
 from deep_stylometry.modules.info_nce_loss import InfoNCELoss
 from deep_stylometry.modules.language_model import LanguageModel
 from deep_stylometry.optimizers import SOAP, SophiaG
@@ -84,7 +85,7 @@ class DeepStylometry(L.LightningModule):
         # Model
         self.lm = LanguageModel(base_model_name, is_decoder_model)
         if contrastive_weight > 0:
-            self.contrastive_loss = InfoNCELoss(
+            self.contrastive_loss = InfoNCELoss(  # ContrastiveLoss(
                 do_late_interaction=do_late_interaction,
                 do_distance=do_distance,
                 exp_decay=exp_decay,