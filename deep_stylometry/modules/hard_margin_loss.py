fix: margin based loss and logits scaling
Move logit scaling to info nce and hybrid loss. Fix a misimplementation
where the margin loss used logits instead of distances.

deep_stylometry/modules/hard_margin_loss.py
@@ -43,16 +43,23 @@ class HardMarginLoss(nn.Module):
             k_mask=k_mask,  # (2B, S)
             gumbel_temp=gumbel_temp,
         )
+        all_dists = 1 - all_scores
 
         targets = torch.arange(batch_size, device=query_embs.device)
-        poss = all_distances[targets, targets]
-        negs = all_distances[targets, targets + batch_size]
+        poss = all_scores[targets, targets]
+        pos_dists = all_dists[targets, targets]
+        negs = all_scores[targets, targets + batch_size]
+        neg_dists = all_dists[targets, targets + batch_size]
 
         # Select hard positive and hard negative pairs
         # Negatives that are too close
-        negative_pairs = negs[negs < (poss.max() if len(poss) > 1 else negs.mean())]
+        negative_pairs = neg_dists[
+            neg_dists < (pos_dists.max() if len(pos_dists) > 1 else neg_dists.mean())
+        ]
         # Positives that are too far
-        positive_pairs = poss[poss > (negs.min() if len(negs) > 1 else poss.mean())]
+        positive_pairs = pos_dists[
+            pos_dists > (neg_dists.min() if len(neg_dists) > 1 else pos_dists.mean())
+        ]
 
         positive_loss = positive_pairs.pow(2).sum()
         negative_loss = F.relu(self.cfg.execution.margin - negative_pairs).pow(2).sum()

deep_stylometry/modules/hybrid_loss.py
@@ -19,11 +19,17 @@ class HybridLoss(nn.Module):
         super().__init__()
         assert cfg.train.margin is not None
         self.cfg = cfg
+        self.tau = nn.Parameter(torch.log(torch.tensor(0.1)))
 
         if cfg.model.pooling_method == "li":
             self.pool = LateInteraction(self.cfg)
         else:
             self.pool = self.mean_pooling
+
+    @property
+    def temperature(self) -> Float[torch.Tensor, ""]:
+        """Exponentiate the scale to get the actual scaling factor."""
+        return torch.exp(self.tau)
 
     def forward(
         self,
@@ -43,15 +49,19 @@ class HybridLoss(nn.Module):
             k_mask=k_mask,  # (2B, S)
             gumbel_temp=gumbel_temp,
         )
+        all_dists = 1 - all_scores
+        all_scores = all_scores / self.temperature
 
         targets = torch.arange(batch_size, device=query_embs.device)
         poss = all_scores[targets, targets]
+        pos_dists = all_dists[targets, targets]
         negs = all_scores[targets, targets + batch_size]
+        neg_dists = all_dists[targets, targets + batch_size]
 
         contrastive_loss = F.cross_entropy(all_scores, targets, reduction="mean")
 
-        positive_loss = poss.pow(2).sum()
-        negative_loss = F.relu(self.cfg.execution.margin - negs).pow(2).sum()
+        positive_loss = pos_dists.pow(2).sum()
+        negative_loss = F.relu(self.cfg.execution.margin - neg_dists).pow(2).sum()
         margin_loss = 0.5 * (positive_loss + negative_loss) / batch_size
 
         loss = contrastive_loss + margin_loss

deep_stylometry/modules/late_interaction.py
@@ -17,7 +17,6 @@ class LateInteraction(nn.Module):
         super().__init__()
         self.cfg = cfg
 
-        self.logit_scale = nn.Parameter(torch.log(torch.tensor(1 / 0.07)))
         self.register_buffer("IGNORE", torch.tensor(float("-inf")))
 
         if self.cfg.model.distance_weightning != "none":
@@ -32,11 +31,6 @@ class LateInteraction(nn.Module):
         """Leaky ReLU ensures alpha >= 0 and has a non-saturating gradient for
         positive values and won't be stuck when it gets slightly below zero."""
         return F.leaky_relu(self.alpha_raw)
-
-    @property
-    def scale(self) -> Float[torch.Tensor, ""]:
-        """Exponentiate the scale to get the actual scaling factor."""
-        return torch.exp(self.logit_scale)
 
     def forward(
         self,
@@ -69,17 +63,13 @@ class LateInteraction(nn.Module):
 
         if not self.cfg.model.use_softmax:
             # Max-based interaction
-            sim_matrix_scaled = self.scale * sim_matrix
-            masked_sim = sim_matrix_scaled.masked_fill(~valid_mask, self.IGNORE)
+            masked_sim = sim_matrix.masked_fill(~valid_mask, self.IGNORE)
             max_sim_values, _ = masked_sim.max(dim=-1)  # (B, B, S)
             scores = (max_sim_values * q_mask.squeeze(1).unsqueeze(1)).sum(dim=-1)
             return scores
 
-        # Scale similarities with learnable logit scale
-        logits = self.scale * sim_matrix
-
         # Mask the padding tokens
-        logits = logits.masked_fill(~valid_mask, self.IGNORE)
+        logits = sim_matrix.masked_fill(~valid_mask, self.IGNORE)
 
         if gumbel_temp is not None:
             p_ij = F.gumbel_softmax(logits, tau=gumbel_temp, hard=False)

deep_stylometry/modules/margin_loss.py
@@ -43,13 +43,16 @@ class MarginLoss(nn.Module):
             k_mask=k_mask,  # (2B, S)
             gumbel_temp=gumbel_temp,
         )
+        all_dists = 1 - all_scores
 
         targets = torch.arange(batch_size, device=query_embs.device)
         poss = all_scores[targets, targets]
+        pos_dists = all_dists[targets, targets]
         negs = all_scores[targets, targets + batch_size]
+        neg_dists = all_dists[targets, targets + batch_size]
 
-        positive_loss = poss.pow(2).sum()
-        negative_loss = F.relu(self.cfg.execution.margin - negs).pow(2).sum()
+        positive_loss = pos_dists.pow(2).sum()
+        negative_loss = F.relu(self.cfg.execution.margin - neg_dists).pow(2).sum()
         loss = 0.5 * (positive_loss + negative_loss) / batch_size
 
         return {

tests/test_deep_stylometry.py
@@ -107,11 +107,13 @@ def test_compute_losses(cfg, batch):
 
     assert "all_scores" in metrics
     assert "targets" in metrics
+    assert "poss" in metrics
+    assert "negs" in metrics
     assert "lm_loss" in metrics
     assert "contrastive_loss" in metrics
     assert "total_loss" in metrics
 
-    assert metrics["all_scores"].shape == (cfg.data.batch_size, 3 * cfg.data.batch_size)
+    assert metrics["all_scores"].shape == (cfg.data.batch_size, 2 * cfg.data.batch_size)
     assert metrics["targets"].shape == (cfg.data.batch_size,)
     assert not torch.isnan(metrics["total_loss"])
     assert metrics["total_loss"] >= 0

tests/test_triplet_loss.py
@@ -37,12 +37,14 @@ def test_li_forward(q_embs, k_embs, q_mask, k_mask):
     cfg.train.margin = 0.5
     loss_fn = TripletLoss(cfg)
 
-    all_scores, targets, loss = loss_fn(q_embs, k_embs, q_mask, k_mask)
+    loss_metrics = loss_fn(q_embs, k_embs, q_mask, k_mask)
 
-    assert not torch.isnan(loss), "InfoNCE loss is NaN"
-    assert loss >= 0
-    assert all_scores.shape == (2, 6), "All scores shape mismatch"
-    assert targets.shape == (2,), "Targets shape mismatch"
+    assert not torch.isnan(loss_metrics["loss"]), "InfoNCE loss is NaN"
+    assert loss_metrics["loss"] >= 0
+    assert loss_metrics["all_scores"].shape == (2, 6), "All scores shape mismatch"
+    assert loss_metrics["targets"].shape == (2,), "Targets shape mismatch"
+    assert loss_metrics["poss"].shape == (2,), "Positive scores shape mismatch"
+    assert loss_metrics["negs"].shape == (2,), "Negative scores shape mismatch"
 
 
 def test_li_exp_decay_forward(q_embs, k_embs, q_mask, k_mask):
@@ -54,11 +56,13 @@ def test_li_exp_decay_forward(q_embs, k_embs, q_mask, k_mask):
     cfg.train.margin = 0.5
     cfg.data.max_length = 128
     loss_fn = TripletLoss(cfg)
-    all_scores, targets, loss = loss_fn(q_embs, k_embs, q_mask, k_mask)
-    assert not torch.isnan(loss), "InfoNCE loss is NaN"
-    assert loss >= 0
-    assert all_scores.shape == (2, 6), "All scores shape mismatch"
-    assert targets.shape == (2,), "Targets shape mismatch"
+    loss_metrics = loss_fn(q_embs, k_embs, q_mask, k_mask)
+    assert not torch.isnan(loss_metrics["loss"]), "InfoNCE loss is NaN"
+    assert loss_metrics["loss"] >= 0
+    assert loss_metrics["all_scores"].shape == (2, 6), "All scores shape mismatch"
+    assert loss_metrics["targets"].shape == (2,), "Targets shape mismatch"
+    assert loss_metrics["poss"].shape == (2,), "Positive scores shape mismatch"
+    assert loss_metrics["negs"].shape == (2,), "Negative scores shape mismatch"
 
 
 def test_li_linear_decay_forward(q_embs, k_embs, q_mask, k_mask):
@@ -70,11 +74,13 @@ def test_li_linear_decay_forward(q_embs, k_embs, q_mask, k_mask):
     cfg.train.margin = 0.5
     cfg.data.max_length = 128
     loss_fn = TripletLoss(cfg)
-    all_scores, targets, loss = loss_fn(q_embs, k_embs, q_mask, k_mask)
-    assert not torch.isnan(loss), "InfoNCE loss is NaN"
-    assert loss >= 0
-    assert all_scores.shape == (2, 6), "All scores shape mismatch"
-    assert targets.shape == (2,), "Targets shape mismatch"
+    loss_metrics = loss_fn(q_embs, k_embs, q_mask, k_mask)
+    assert not torch.isnan(loss_metrics["loss"]), "InfoNCE loss is NaN"
+    assert loss_metrics["loss"] >= 0
+    assert loss_metrics["all_scores"].shape == (2, 6), "All scores shape mismatch"
+    assert loss_metrics["targets"].shape == (2,), "Targets shape mismatch"
+    assert loss_metrics["poss"].shape == (2,), "Positive scores shape mismatch"
+    assert loss_metrics["negs"].shape == (2,), "Negative scores shape mismatch"
 
 
 def test_li_softmax_forward(q_embs, k_embs, q_mask, k_mask):
@@ -84,11 +90,13 @@ def test_li_softmax_forward(q_embs, k_embs, q_mask, k_mask):
     cfg.model.distance_weightning = "none"
     cfg.train.margin = 0.5
     loss_fn = TripletLoss(cfg)
-    all_scores, targets, loss = loss_fn(q_embs, k_embs, q_mask, k_mask)
-    assert not torch.isnan(loss), "InfoNCE loss is NaN"
-    assert loss >= 0
-    assert all_scores.shape == (2, 6), "All scores shape mismatch"
-    assert targets.shape == (2,), "Targets shape mismatch"
+    loss_metrics = loss_fn(q_embs, k_embs, q_mask, k_mask)
+    assert not torch.isnan(loss_metrics["loss"]), "InfoNCE loss is NaN"
+    assert loss_metrics["loss"] >= 0
+    assert loss_metrics["all_scores"].shape == (2, 6), "All scores shape mismatch"
+    assert loss_metrics["targets"].shape == (2,), "Targets shape mismatch"
+    assert loss_metrics["poss"].shape == (2,), "Positive scores shape mismatch"
+    assert loss_metrics["negs"].shape == (2,), "Negative scores shape mismatch"
 
 
 def test_li_gumbel_softmax_forward(q_embs, k_embs, q_mask, k_mask):
@@ -99,11 +107,13 @@ def test_li_gumbel_softmax_forward(q_embs, k_embs, q_mask, k_mask):
     cfg.model.initial_gumbel_temp = 0.5
     cfg.train.margin = 0.5
     loss_fn = TripletLoss(cfg)
-    all_scores, targets, loss = loss_fn(q_embs, k_embs, q_mask, k_mask, gumbel_temp=0.5)
-    assert not torch.isnan(loss), "InfoNCE loss is NaN"
-    assert loss >= 0
-    assert all_scores.shape == (2, 6), "All scores shape mismatch"
-    assert targets.shape == (2,), "Targets shape mismatch"
+    loss_metrics = loss_fn(q_embs, k_embs, q_mask, k_mask)
+    assert not torch.isnan(loss_metrics["loss"]), "InfoNCE loss is NaN"
+    assert loss_metrics["loss"] >= 0
+    assert loss_metrics["all_scores"].shape == (2, 6), "All scores shape mismatch"
+    assert loss_metrics["targets"].shape == (2,), "Targets shape mismatch"
+    assert loss_metrics["poss"].shape == (2,), "Positive scores shape mismatch"
+    assert loss_metrics["negs"].shape == (2,), "Negative scores shape mismatch"
 
 
 def test_mean_pooling_forward(q_embs, k_embs, q_mask, k_mask):
@@ -111,8 +121,10 @@ def test_mean_pooling_forward(q_embs, k_embs, q_mask, k_mask):
     cfg.model.pooling_method = "mean"
     cfg.train.margin = 0.5
     loss_fn = TripletLoss(cfg)
-    all_scores, targets, loss = loss_fn(q_embs, k_embs, q_mask, k_mask)
-    assert not torch.isnan(loss), "InfoNCE loss is NaN"
-    assert loss >= 0
-    assert all_scores.shape == (2, 6), "All scores shape mismatch"
-    assert targets.shape == (2,), "Targets shape mismatch"
+    loss_metrics = loss_fn(q_embs, k_embs, q_mask, k_mask)
+    assert not torch.isnan(loss_metrics["loss"]), "InfoNCE loss is NaN"
+    assert loss_metrics["loss"] >= 0
+    assert loss_metrics["all_scores"].shape == (2, 6), "All scores shape mismatch"
+    assert loss_metrics["targets"].shape == (2,), "Targets shape mismatch"
+    assert loss_metrics["poss"].shape == (2,), "Positive scores shape mismatch"
+    assert loss_metrics["negs"].shape == (2,), "Negative scores shape mismatch"