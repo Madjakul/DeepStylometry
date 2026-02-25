f2fd72be89f066cf24288543d8152ea2a2c28261
Madjakul <Madjakul@users.noreply.github.com>
2026-02-25 23:15:54 +0000

    refactor: apply code formatting

--- deep_stylometry/callbacks/__init__.py
+++ deep_stylometry/callbacks/__init__.py
@@ -2,11 +2,11 @@
 
 from deep_stylometry.callbacks.eval_runtime_monitor import EvalRuntimeMonitor
 from deep_stylometry.callbacks.grad_norm_monitor import GradNormMonitor
-from deep_stylometry.callbacks.logarithmic_validation_callback import (
-    LogarithmicValidationCallback,
-)
+from deep_stylometry.callbacks.logarithmic_validation_callback import \
+    LogarithmicValidationCallback
 from deep_stylometry.callbacks.loss_variance_monitor import LossVarianceMonitor
-from deep_stylometry.callbacks.retrieval_eval_callback import RetrievalEvalCallback
+from deep_stylometry.callbacks.retrieval_eval_callback import \
+    RetrievalEvalCallback
 from deep_stylometry.callbacks.test_eval_callback import TestEvalCallback
 
 __all__ = [
--- deep_stylometry/utils/argparsers/__init__.py
+++ deep_stylometry/utils/argparsers/__init__.py
@@ -1,6 +1,6 @@
 # deep_strylometry/utils/argparsers/__init__.py
 
-from deep_stylometry.utils.argparsers.train_argparse import TrainArgparse
 from deep_stylometry.utils.argparsers.test_argparse import TestArgparse
+from deep_stylometry.utils.argparsers.train_argparse import TrainArgparse
 
 __all__ = ["TrainArgparse", "TestArgparse"]
