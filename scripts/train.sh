a023cb683a1d2104c53be54b820faae79c35ce22
Madjakul <francis.kulumba@inria.fr>
2026-02-27 12:17:10 +0100

    feat: training scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,20 +5,24 @@ DATA_ROOT=$PROJECT_ROOT/data                     # Do not modify
 
 # ************************** Customizable Arguments ************************************
 
-SLURM=true
 CONFIG_PATH=$PROJECT_ROOT/configs/train.yml
+PROCESSED_DS_DIR=$SCRATCH/Datasets/deep-stylometry/answerdotai-modernbert-base/no-padding/
 LOGS_DIR=$PROJECT_ROOT/logs
 
 # --------------------------------------------------------------------------------------
 
-# CACHE_DIR=$DATA_ROOT/responses/
-CHECKPOINT_DIR=$PROJECT_ROOT/tmp/checkpoints/
-# CHECKPOINT_PATH=$CHECKPOINT_DIR/FacebookAI-roberta-base-se-pooling:li-softmax:True-gumbel:1-dist:none/epoch=1.ckpt
+# CACHE_DIR=$SCRATCH/cache
+CHECKPOINT_DIR=$PROJECT_ROOT/tmp/
 NUM_PROC=10
 
 # **************************************************************************************
 
-if [[ "$SLURM" == "true" ]]; then
+mkdir -p "$LOGS_DIR" || true
+if [[ -v CHECKPOINT_DIR ]]; then
+    mkdir -p "$CHECKPOINT_DIR" || true
+fi
+
+if [[ $SLURM_JOB_ID != "" ]]; then
     echo "SLURM_JOB_ID: $SLURM_JOB_ID"
     echo "SLURM_JOB_NODELIST: $SLURM_JOB_NODELIST"
     echo "SLURM_NNODES: $SLURM_NNODES"
@@ -33,15 +37,16 @@ if [[ "$SLURM" == "true" ]]; then
         --nproc_per_node=$SLURM_GPUS_ON_NODE \
         "$PROJECT_ROOT/train.py" \
         --config_path "$CONFIG_PATH" \
+        --processed_ds_dir "$PROCESSED_DS_DIR" \
         --logs_dir "$LOGS_DIR" \
         ${CACHE_DIR:+--cache_dir "$CACHE_DIR"} \
         ${CHECKPOINT_DIR:+--checkpoint_dir "$CHECKPOINT_DIR"} \
-        ${CHECKPOINT_PATH:+--checkpoint_path "$CHECKPOINT_PATH"} \
         ${NUM_PROC:+--num_proc "$NUM_PROC"}
 else
     cmd=()
     cmd+=(python3 "$PROJECT_ROOT/train.py"
         --config_path "$CONFIG_PATH"
+        --processed_ds_dir "$PROCESSED_DS_DIR"
         --logs_dir "$LOGS_DIR")
 
     if [[ -v CACHE_DIR ]]; then
@@ -52,10 +57,6 @@ else
     if [[ -v CHECKPOINT_DIR ]]; then
         mkdir -p "$CHECKPOINT_DIR" || true
         cmd+=(--checkpoint_dir "$CHECKPOINT_DIR")
-    fi
-
-    if [[ -v CHECKPOINT_PATH ]]; then
-        cmd+=(--checkpoint_path "$CHECKPOINT_PATH")
     fi
 
     if [[ -v NUM_PROC ]]; then
--- train.py
+++ train.py
@@ -3,7 +3,6 @@
 import logging
 import os
 
-import psutil
 
 from deep_stylometry.modules import DeepStylometry
 from deep_stylometry.utils import train_utils
@@ -14,7 +13,6 @@ from deep_stylometry.utils.logger import logging_config
 
 os.environ["PYTHONUNBUFFERED"] = "1"
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
-NUM_PROC = psutil.cpu_count(logical=False)
 
 
 set_seed()
@@ -28,48 +26,20 @@ if __name__ == "__main__":
     logging.info("Preparing data module...")
     dm = train_utils.setup_datamodule(
         cfg=cfg,
+        processed_ds_dir=args.processed_ds_dir,
+        num_proc=args.num_proc,
         cache_dir=args.cache_dir,
-        num_proc=args.num_proc if args.num_proc is not None else NUM_PROC,
     )
 
-    if cfg.do_test:
-        logging.info("Setting up DataModule for test split...")
-        dm.setup("test")
+    logging.info("=== Fine-tuning ===")
+    model = DeepStylometry(cfg)
 
-    model = None
-    trainer = None
+    trainer = train_utils.setup_trainer(
+        cfg=cfg,
+        model=model,
+        logs_dir=args.logs_dir,
+        checkpoint_dir=args.checkpoint_dir,
+    )
 
-    # Training
-    if cfg.do_train:
-        logging.info("--- Fine-tuning ---")
-        model = DeepStylometry(cfg)
-
-        trainer = train_utils.setup_trainer(
-            cfg=cfg,
-            model=model,
-            logs_dir=args.logs_dir,
-            checkpoint_dir=args.checkpoint_dir,
-        )
-
-        trainer.fit(model=model, datamodule=dm)
-        logging.info("--- Fine-tuning finished ---")
-
-    # Testing
-    if cfg.do_test:
-        logging.info("--- Testing ---")
-        if trainer is None:
-            if args.checkpoint_path is not None:
-                logging.info("Loading DeepStylometry model from checkpoint.")
-                model = DeepStylometry.load_from_checkpoint(args.checkpoint_path)
-            else:
-                model = DeepStylometry(cfg)
-
-            trainer = train_utils.setup_trainer(
-                cfg=cfg,
-                model=model,
-                logs_dir=args.logs_dir,
-                checkpoint_dir=None,
-            )
-
-        trainer.test(model=model, datamodule=dm)
-        logging.info("--- Testing finished ---")
+    trainer.fit(model=model, datamodule=dm)
+    logging.info("=== Fine-tuning finished ===")
