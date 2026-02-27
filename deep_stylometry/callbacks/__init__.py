e9802bb7f028b4ba920ec97924df7e46dcf3adee
Madjakul <Madjakul@users.noreply.github.com>
2026-02-27 23:21:50 +0000

    refactor: apply code formatting

--- deep_stylometry/callbacks/__init__.py
+++ deep_stylometry/callbacks/__init__.py
@@ -1,11 +1,11 @@
 # deep_stylometry/callbacks/__init__.py
 
 from deep_stylometry.callbacks.eval_runtime_monitor import EvalRuntimeMonitor
-from deep_stylometry.callbacks.logarithmic_validation_callback import (
-    LogarithmicValidationCallback,
-)
+from deep_stylometry.callbacks.logarithmic_validation_callback import \
+    LogarithmicValidationCallback
 from deep_stylometry.callbacks.loss_variance_monitor import LossVarianceMonitor
-from deep_stylometry.callbacks.retrieval_eval_callback import RetrievalEvalCallback
+from deep_stylometry.callbacks.retrieval_eval_callback import \
+    RetrievalEvalCallback
 from deep_stylometry.callbacks.test_eval_callback import TestEvalCallback
 
 __all__ = [
