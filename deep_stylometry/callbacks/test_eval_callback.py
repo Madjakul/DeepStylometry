71a1a453666e9a84471d2b352aa496e15897f271
Madjakul <Madjakul@users.noreply.github.com>
2026-03-04 14:32:01 +0000

    refactor: apply code formatting

--- deep_stylometry/callbacks/test_eval_callback.py
+++ deep_stylometry/callbacks/test_eval_callback.py
@@ -16,7 +16,8 @@ from ranx import Run
 
 from deep_stylometry.modules.late_interaction import LateInteraction
 from deep_stylometry.modules.mean_interaction import MeanInteraction
-from deep_stylometry.utils.eval_utils import build_qrels, evaluate_run, gather_targets
+from deep_stylometry.utils.eval_utils import (build_qrels, evaluate_run,
+                                              gather_targets)
 
 
 class TestEvalCallback(L.Callback):
