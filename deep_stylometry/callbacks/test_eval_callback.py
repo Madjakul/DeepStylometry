44de692914a154b5e3213b3cede7f13b12e0ad1e
Madjakul <Madjakul@users.noreply.github.com>
2026-03-07 19:01:26 +0000

    refactor: apply code formatting

--- deep_stylometry/callbacks/test_eval_callback.py
+++ deep_stylometry/callbacks/test_eval_callback.py
@@ -15,7 +15,8 @@ from tqdm import tqdm
 
 from deep_stylometry.modules.late_interaction import LateInteraction
 from deep_stylometry.modules.mean_interaction import MeanInteraction
-from deep_stylometry.utils.eval_utils import build_qrels, evaluate_run, gather_targets
+from deep_stylometry.utils.eval_utils import (build_qrels, evaluate_run,
+                                              gather_targets)
 
 if TYPE_CHECKING:
     from deep_stylometry.utils.configs import BaseConfig
