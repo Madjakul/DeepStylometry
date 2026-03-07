5a49c0cb5d56543a56c5c511a7876005006a8582
Madjakul <francis.kulumba@inria.fr>
2026-03-07 19:38:53 +0100

    refactor: more explicit naming scheme

--- test.py
+++ test.py
@@ -34,8 +34,9 @@ if __name__ == "__main__":
 
     name = (
         (
-            f"{cfg.model.base_checkpoint}__{cfg.data.ds_name}"
+            f"test__{cfg.model.base_checkpoint}__{cfg.data.ds_name}"
             f"__pooling-{cfg.model.pooling_method}__{cfg.data.test_subset}"
+            f"skip_list-{cfg.model.skip_list}-true"
         )
         .replace("/", "-")
         .lower()
